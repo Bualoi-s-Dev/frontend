031f2baf1a0513abbef2c6d434b8efbb28c022df
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-02-06 22:13:10 +0700

    add: back button component

---
+++ components/BackButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const router = useRouter();
+
+const props = defineProps<{
+    onClick?: () => void;
+}>();
+const clickHandler = computed(() => props.onClick ?? (() => router.back()))
+</script>
+
+<template>
+    <button class="w-8 h-8 flex items-center justify-center cursor-pointer rounded-lg border border-stroke"
+        @click="clickHandler">
+        <svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6.96753 12.5604L0.874754 6.46765L6.96753 0.374877" stroke="#14142B" />
+        </svg>
+    </button>
+</template>
