040c2f507ba910caea62bb78ae6236c0745e8bea
Nontanun Ausungnoen <nunx111@gmail.com>
2025-02-09 23:39:12 +0700

    feat: add work card and work list of cards

--- components/WorkList.vue
+++ components/WorkList.vue
@@ -1,49 +1,31 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  data: {
+    type: Array as () => Array<{
+      images: string[];
+      title: string;
+      owner: string;
+      type: string;
+      editable?: boolean;
+      addable?: boolean;
+    }>,
+    required: true,
+  },
+});
+</script>
+
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      <ImageCard 
-        v-for="(item, index) in items" 
-        :key="index" 
-        :images="item.images" 
-        :title="item.title" 
-        :owner="item.owner" 
-        :type="item.type" 
-      />
-    </div>
-  </template>
-  
-  <script>
-  import ImageCard from "@/components/WorkCard.vue";
-  import workImage from "@/assets/workImage.png"
-  import workImage2 from "@/assets/workImage2.png"
-  export default {
-    name: "ImageCardList",
-    components: {
-      ImageCard,
-    },
-    data() {
-      return {
-        items: [
-          {
-            images: [workImage, workImage2],
-            title: "Beautiful Sunset",
-            owner: "John Doe",
-            type: "Nature",
-          },
-          {
-            images: [workImage, workImage2],
-            title: "City Lights",
-            owner: "Jane Smith",
-            type: "Urban",
-          },
-          {
-            images: [workImage, workImage2],
-            title: "Mountain View",
-            owner: "Alice Johnson",
-            type: "Adventure",
-          },
-        ],
-      };
-    },
-  };
-  </script>
-  
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+    <WorkCard
+      v-for="(work, index) in data"
+      :key="index"
+      :images="work.images"
+      :title="work.title"
+      :owner="work.owner"
+      :type="work.type"
+      :editable="work.editable"
+      :addable="work.addable"
+    />
+  </div>
+</template>
