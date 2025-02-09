875eeb409de102132a07c8424af8a0d7c39df6bd
Nontanun Ausungnoen <nunx111@gmail.com>
2025-02-09 15:04:01 +0700

    feat: add new customization button options

--- components/Button.vue
+++ components/Button.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
     leftIcon?: string | Record<string, any>,
     rightIcon?: string | Record<string, any>,
-    bgColor?: string,       // Background color
-    textColor?: string,     // Text color
-    fontSize?: string,      // Font size
-    fontFamily?: string     // Font family
-}>()
-
-// Dynamic class computation
-const buttonClasses = computed(() => {
-    const bg = props.bgColor || 'bg-black';
-    const text = props.textColor || 'text-white';
-    const size = props.fontSize || 'text-sm';
-    const font = props.fontFamily || 'font-sans';
-
-    return `inline-flex items-center rounded min-h-[32px] px-3 py-0.5 font-semibold 
-        hover:brightness-90 transition duration-200 ${bg} ${text} ${size} ${font}`;
+    bgColor?: string,       
+    width?: string,         
+    height?: string,        
+    textOptions?: string    // Handles text size, alignment, weight, color, etc.
+}>(), {
+    bgColor: 'bg-black',
+    width: 'w-auto',   
+    height: 'h-8',
+    textOptions: 'text-sm text-center font-semibold text-white'  // Default text styles (includes color)
 });
+
+const buttonClasses = computed(() => 
+    `inline-flex items-center justify-center rounded px-3 py-0.5 transition duration-200 
+    hover:brightness-90 
+    ${props.bgColor} ${props.width} ${props.height} ${props.textOptions}`
+);
 </script>
 
 <template>
-    <button :class="buttonClasses">
-        <!-- Left Icon -->
-        <img v-if="typeof props.leftIcon === 'string'" :src="props.leftIcon" class="w-5 h-5 mr-2" />
-        <component v-else-if="props.leftIcon" :is="props.leftIcon" class="w-5 h-5 mr-2" />
+    <button :class="buttonClasses" type="button">
+        <slot name="leftIcon">
+            <img v-if="typeof props.leftIcon === 'string'" :src="props.leftIcon" class="w-5 h-5 mr-2" />
+            <component v-else-if="props.leftIcon" :is="props.leftIcon" class="w-5 h-5 mr-2" />
+        </slot>
 
         <slot />
 
-        <!-- Right Icon -->
-        <img v-if="typeof props.rightIcon === 'string'" :src="props.rightIcon" class="w-5 h-5 ml-2" />
-        <component v-else-if="props.rightIcon" :is="props.rightIcon" class="w-5 h-5 ml-2" />
+        <slot name="rightIcon">
+            <img v-if="typeof props.rightIcon === 'string'" :src="props.rightIcon" class="w-5 h-5 ml-2" />
+            <component v-else-if="props.rightIcon" :is="props.rightIcon" class="w-5 h-5 ml-2" />
+        </slot>
     </button>
 </template>
