40c9881ba0c25acf1b30d7b53758be0bb65640ad
neonaht <71958540+neonaht@users.noreply.github.com>
2025-02-09 21:05:13 +0700

    Feat: dynamic routes for login/reset

--- pages/user/login/reset/[id].vue
+++ pages/user/login/reset/[id].vue
@@ -13,30 +13,21 @@ import { Icon } from '@iconify/vue';
 import LoginRegisterCard from "~/components/LoginRegisterCard.vue";
 
 const router = useRouter();
+const route = useRoute();
 const email = ref("");
-const password = ref("");
-const confirmPassword = ref("");
 const errorMessage = ref("");
-const steps = [1, 2, 3, 4];
-const currentStep = 1;
+const pages = [1, 2, 3, 4];
+const currentPage = Number(route.params.id);
 
 // Email validation (simple regex)
 const isEmailValid = computed(() =>
   /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)
 );
 
-// Password validation (min 6 chars)
-const isPasswordValid = computed(() => password.value.length >= 6);
-
-// Confirm password validation
-const isConfirmPasswordValid = computed(
-  () => password.value === confirmPassword.value
-);
-
 // Form validation
 const isFormValid = computed(
   () =>
-    isEmailValid.value && isPasswordValid.value && isConfirmPasswordValid.value
+    isEmailValid.value
 );
 
 // Handle form submission
@@ -47,66 +38,100 @@ const handleSubmit = () => {
   }
 
   console.log("Email:", email.value);
-  console.log("Password:", password.value);
-  console.log("Confirm Password:", confirmPassword.value);
   errorMessage.value = ""; // Clear errors if successful
 
-  router.push("/user/register/selectRole");
+  router.push(`/user/login/reset/${currentPage % 4 + 1}`);
 };
 </script>
 
 <template>
-  <LoginRegisterCard>
-    <div class="flex flex-row items-end gap-3 font-extrabold text-xl">
-      <img :src="logo" alt="logo" class="w-9" />
-      Photomatch
-    </div>
-    <div class="flex flex-col gap-[20px]">
-      <div class="flex flex-col gap-[16px]">
-        <div>
-            <div class="mb-3">
-                <p class="text-lg">Forgot your password?</p>
+    <LoginRegisterCard>
+        <div class="flex flex-row items-end gap-3 font-extrabold text-xl">
+            <img :src="logo" alt="logo" class="w-9" />
+            Photomatch
+        </div>
+        <template v-if="currentPage == 1">
+            <div class="flex flex-col gap-[20px]">
+                <div class="flex flex-col gap-[16px]">
+                    <div>
+                        <div class="mb-3">
+                            <p class="text-lg">Forgot your password?</p>
+                        </div>
+                        <p class="text-sm/6">Please enter your email to reset the password</p>
+                        <label>Email<span class="text-red-500">*</span></label>
+                        <input
+                            v-model="email"
+                            type="email"
+                            class="border w-full rounded-md py-[6px] px-2 text-[14px] border-stroke"
+                        />
+                        <p v-if="email && !isEmailValid" class="text-red-500 text-xs">
+                            Invalid email format
+                        </p>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-[16px]">
+                    <Button
+                    class="flex items-center justify-center py-[12px]"
+                    textOptions="text-white text-[14px] font-poppins"
+                    @click="handleSubmit"
+                    >Reset Password
+                    </Button>
+                </div>
             </div>
-            <p class="text-sm/6">Please enter your email to reset the password</p>
-            <label>Email<span class="text-red-500">*</span></label>
-            <input
-                v-model="email"
-                type="email"
-                class="border w-full rounded-md py-[6px] px-2 text-[14px] border-stroke"
-            />
-            <p v-if="email && !isEmailValid" class="text-red-500 text-xs">
-                Invalid email format
-            </p>
+        </template>
+        <template v-else-if="currentPage == 2">
+            <div class="flex flex-col gap-[20px]">
+                <div class="flex flex-col gap-[16px]">
+                    <div>
+                        <div class="mb-3">
+                            <p class="text-lg">Check your email</p>
+                        </div>
+                        <p class="text-sm/6">We sent a reset link to 653xxxxx21@student.chula.ac.th enter 5 digit code that mentioned in the email</p>
+                        <div class="flex space-x-2 p-4">
+                            <div class="w-12 h-12 flex items-center justify-center border border-black rounded-lg text-lg font-semibold">
+                                8
+                            </div>
+                            <div class="w-12 h-12 flex items-center justify-center border border-black rounded-lg text-lg font-semibold">
+                                5
+                            </div>
+                            <div class="w-12 h-12 flex items-center justify-center border border-gray-300 rounded-lg text-lg text-gray-400">
+                                &nbsp;
+                            </div>
+                            <div class="w-12 h-12 flex items-center justify-center border border-gray-300 rounded-lg text-lg text-gray-400">
+                                &nbsp;
+                            </div>
+                            <div class="w-12 h-12 flex items-center justify-center border border-gray-300 rounded-lg text-lg text-gray-400">
+                                &nbsp;
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-[16px]">
+                    <Button
+                    class="flex items-center justify-center py-[12px]"
+                    textOptions="text-white text-[14px] font-poppins"
+                    @click="handleSubmit"
+                    >Verify Code
+                    </Button>
+                </div>
+            </div>
+        </template>
+        <div class="flex items-center justify-center">
+            <div
+            v-for="step in pages"
+            :key="step"
+            class="flex items-center"
+            >
+            <div
+                class="w-10 h-10 flex items-center justify-center rounded-full border-2"
+                :class="step === currentPage ? 'bg-black text-white border-black' : 'bg-white text-black border-black'"
+            >
+                {{ step }}
+            </div>
+            <div v-if="step !== pages.length" class="w-5 h-1 bg-black"></div>
+            </div>
         </div>
-      </div>
-      <div class="flex flex-col gap-[16px]">
-        <Button
-          class="flex items-center justify-center py-[12px]"
-          textOptions="text-white text-[14px] font-poppins"
-          @click="handleSubmit"
-          >Reset Password
-        </Button>
-      </div>
-    </div>
-    <div class="flex items-center justify-center">
-        <div
-        v-for="step in steps"
-        :key="step"
-        class="flex items-center"
-        >
-        <!-- Circle -->
-        <div
-            class="w-10 h-10 flex items-center justify-center rounded-full border-2"
-            :class="step === currentStep ? 'bg-black text-white border-black' : 'bg-white text-black border-black'"
-        >
-            {{ step }}
-        </div>
-
-        <!-- Line -->
-        <div v-if="step !== steps.length" class="w-5 h-1 bg-black"></div>
-        </div>
-    </div>
-  </LoginRegisterCard>
+    </LoginRegisterCard>
 </template>
 
 
