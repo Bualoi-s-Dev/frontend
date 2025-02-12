0c3d1a2a33132ff5d5503e7c7154531fb5acb497
Wiroon <wiroonpuri123@gmail.com>
2025-02-12 18:59:11 +0700

    fix: delete Google & Facebook register button

--- pages/user/register/index.vue
+++ pages/user/register/index.vue
@@ -7,7 +7,6 @@ import { useRouter } from "vue-router";
 import logo from "assets/logo.png";
 
 import LoginRegisterCard from "~/components/LoginRegisterCard.vue";
-import axios from "axios";
 
 const responseData = ref(null);
 const router = useRouter();
@@ -52,23 +51,6 @@ const handleSubmit = () => {
     path: "/user/register/createUser",
     query: { email: email.value },
   });
-};
-
-const varToken =
-  "eyJhbGciOiJSUzI1NiIsImtpZCI6IjhkMjUwZDIyYTkzODVmYzQ4NDJhYTU2YWJhZjUzZmU5NDcxNmVjNTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vc2UtMi03NmY2MiIsImF1ZCI6InNlLTItNzZmNjIiLCJhdXRoX3RpbWUiOjE3MzkzNTE2OTMsInVzZXJfaWQiOiJGeWQzMmhEU2dUUTNLNTE2aHRQSklIQ0RzeWcyIiwic3ViIjoiRnlkMzJoRFNnVFEzSzUxNmh0UEpJSENEc3lnMiIsImlhdCI6MTczOTM1MTY5MywiZXhwIjoxNzM5MzU1MjkzLCJlbWFpbCI6Indpcm9vbnB1cmkxMjNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbIndpcm9vbnB1cmkxMjNAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.cQY-9MRBUcwevFFFOEIWipZEuY9Kasa55fDiKar2v57-i_207qIy87h4LMNqlYNYMJBHGgBfNyMs11P2nB2XSmLSPi9k35eb_76Z2MJxW37rYu935hkyL8ZEt-NROLhgvn3qPVSW3Cx9GkuNhJIjlkXThitEnpcF-YBJCpKDywef2X-LmUT1vjRNVlPrI5z4OMJe6HCSEmXcx-U6aKvKTm4p2WOaRG2Mj22T9Mbu6YGaru7Az4Em55woIgfXvWdHF-Vgx-za6h2KilflLP4_vM8mHb0PMi8Xd_3HqQ60QwIz7GQJjwWKFABmypWV7Ak5FRhbtdwQPSnwYgYx2zQE1g";
-
-const fetchUserProfile = async () => {
-  try {
-    const response = await axios.get("http://localhost:8080/user/me", {
-      headers: {
-        Authorization: "Bearer " + varToken,
-      },
-    });
-    console.log(response.data);
-    responseData.value = response.data;
-  } catch (error: any) {
-    console.error("Error fetching profile:", error.message);
-  }
 };
 </script>
 
@@ -127,31 +109,6 @@ const fetchUserProfile = async () => {
         @click="handleSubmit"
       >
         Register
-      </Button>
-
-      <!-- Fetch User Profile Button -->
-      <Button
-        class="flex items-center justify-center py-[18px] bg-blue-500"
-        textOptions="text-white text-[14px] font-poppins"
-        @click="fetchUserProfile"
-      >
-        Fetch User Profile
-      </Button>
-
-      <Button
-        class="flex items-center justify-center py-[18px] bg-google"
-        textOptions="text-titleActive text-[14px] font-poppins"
-        :leftIcon="'flat-color-icons:google'"
-      >
-        Register With Google
-      </Button>
-
-      <Button
-        class="flex items-center justify-center py-[18px] bg-facebook"
-        textOptions="text-white text-[14px] font-poppins"
-        :leftIcon="'logos:facebook'"
-      >
-        Register With Facebook
       </Button>
     </div>
 
