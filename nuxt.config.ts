1e09d5e8b5a4dd3b114a5319ec46053dee86ca39
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-02-12 16:36:35 +0700

    build: set runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      firebaseConfig: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      },
+      apiUrl: process.env.API_URL
+    }
+  },
   components: [
     {
       path: '~/components',
