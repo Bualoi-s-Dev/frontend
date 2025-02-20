ae0e69b2705508e51020453df795dc32c2b50e2c
Wiroon <wiroonpuri123@gmail.com>
2025-02-20 17:52:53 +0700

    fix: change HTTP method from put to patch

--- stores/api.ts
+++ stores/api.ts
@@ -1,7 +1,7 @@
-import { defineStore } from 'pinia';
-import axios from 'axios';
-import type { Package, PackageStrictRequest, User } from '~/types/api';
-import { useAuthStore } from './auth';
+import { defineStore } from "pinia";
+import axios from "axios";
+import type { Package, PackageStrictRequest, User } from "~/types/api";
+import { useAuthStore } from "./auth";
 
 export const useApiStore = defineStore("api", () => {
   const auth = useAuthStore();
@@ -14,49 +14,57 @@ export const useApiStore = defineStore("api", () => {
     return response.data as User;
   };
 
-    const createPackage = async (pkg: PackageStrictRequest) => {
-        // TODO: remove trailing slash when backend fix the endpoint.
-        const response = await axios.post(`${config.public.apiUrl}/package/`, pkg, {
-            headers: { Authorization: `Bearer ${await auth.fetchToken()}` }
-        })
-    }
+  const createPackage = async (pkg: PackageStrictRequest) => {
+    // TODO: remove trailing slash when backend fix the endpoint.
+    const response = await axios.post(`${config.public.apiUrl}/package/`, pkg, {
+      headers: { Authorization: `Bearer ${await auth.fetchToken()}` },
+    });
+  };
 
-    const updatePackage = async (id: string, pkg: PackageStrictRequest) => {
-        console.log('updating package')
-        const response = await axios.patch(`${config.public.apiUrl}/package/${id}`, pkg, {
-            headers: { Authorization: `Bearer ${await auth.fetchToken()}` }
-        })
-        console.log(pkg.photos[0], 'update package')
-        return response.data;
-    }
+  const updatePackage = async (id: string, pkg: PackageStrictRequest) => {
+    console.log("updating package");
+    const response = await axios.patch(
+      `${config.public.apiUrl}/package/${id}`,
+      pkg,
+      {
+        headers: { Authorization: `Bearer ${await auth.fetchToken()}` },
+      }
+    );
+    console.log(pkg.photos[0], "update package");
+    return response.data;
+  };
 
-    const fetchUserPackage = async (): Promise<Package[]> => {
-        // TODO: remove trailing slash when backend fix the endpoint.
-        const response = await axios.get(`${config.public.apiUrl}/package/`, {
-            headers: { Authorization: `Bearer ${await auth.fetchToken()}` }
-        });
-        return response.data as Package[];
-    }
+  const fetchUserPackage = async (): Promise<Package[]> => {
+    // TODO: remove trailing slash when backend fix the endpoint.
+    const response = await axios.get(`${config.public.apiUrl}/package/`, {
+      headers: { Authorization: `Bearer ${await auth.fetchToken()}` },
+    });
+    return response.data as Package[];
+  };
 
-    const fetchPackage = async (id: string): Promise<Package> => {
-        const response = await axios.get(`${config.public.apiUrl}/package/${id}`, {
-            headers: { Authorization: `Bearer ${await auth.fetchToken()}` }
-        });
-        return response.data as Package;
-    }
+  const fetchPackage = async (id: string): Promise<Package> => {
+    const response = await axios.get(`${config.public.apiUrl}/package/${id}`, {
+      headers: { Authorization: `Bearer ${await auth.fetchToken()}` },
+    });
+    return response.data as Package;
+  };
 
-    const updateUserInformation = async (payload: any) => {
-        const response = await axios.put(`${config.public.apiUrl}/user/profile`, payload, {
-            headers: { Authorization: `Bearer ${await auth.fetchToken()}` }
-        });
-    }
+  const updateUserInformation = async (payload: any) => {
+    const response = await axios.patch(
+      `${config.public.apiUrl}/user/profile`,
+      payload,
+      {
+        headers: { Authorization: `Bearer ${await auth.fetchToken()}` },
+      }
+    );
+  };
 
-    return {
-        fetchPackage,
-        fetchUserPackage,
-        fetchUserProfile,
-        createPackage,
-        updatePackage,
-        updateUserInformation
-    };
+  return {
+    fetchPackage,
+    fetchUserPackage,
+    fetchUserProfile,
+    createPackage,
+    updatePackage,
+    updateUserInformation,
+  };
 });
