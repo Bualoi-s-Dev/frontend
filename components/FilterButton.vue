37e4794eb2797be785bdc63ca1afbaa047e64c8f
Wiroon <wiroonpuri123@gmail.com>
2025-02-26 03:43:13 +0700

    add: filter button UI

---
+++ components/FilterButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+</script>
+<template>
+  <div
+    class="flex items-center justify-center p-[10px] border border-stroke rounded-[6px]"
+  >
+    <Icon icon="solar:filter-linear" class="w-[16px] h-[16px] text-black" />
+  </div>
+</template>
