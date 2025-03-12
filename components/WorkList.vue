ba929042b15aa7a1438b0948a4bd984d1f8a8e07
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-03-12 17:45:37 +0700

    feat: package in profile page route to package page

--- components/WorkList.vue
+++ components/WorkList.vue
@@ -1,31 +1,25 @@
 <script setup lang="ts">
-  const props = defineProps<{
-    data: {
-        id?: string;
-        photoUrls: string[];
-        title: string;
-        owner?: string;
-        type: string;
-        editable?: boolean;
-        addable?: boolean;
-      }[];
-  }>();
+const props = defineProps<{
+  data: {
+    id?: string;
+    photoUrls: string[];
+    title: string;
+    owner?: string;
+    type: string;
+    editable?: boolean;
+    addable?: boolean;
+  }[];
+  navigate?: boolean;
+}>();
 
-  const config = useRuntimeConfig();
+const config = useRuntimeConfig();
 </script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
     <div v-for="(work, index) in data" :key="index">
-      <WorkCard
-        :images="work.photoUrls.map(url => `${config.public.s3URL}${url}`)"
-        :title="work.title"
-        :owner="work.owner"
-        :type="work.type"
-        :id="work.id"
-        editable
-        :addable="work.addable"
-      />
+      <WorkCard :navigate="navigate" :images="work.photoUrls.map(url => `${config.public.s3URL}${url}`)"
+        :title="work.title" :owner="work.owner" :type="work.type" :id="work.id" editable :addable="work.addable" />
     </div>
   </div>
 </template>
