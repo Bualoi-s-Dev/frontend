6362bb6ef20864e9cb858f5bdb5fdadc202bb331
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-03-21 00:41:01 +0700

    feat: add photographer card component

---
+++ components/PhotographerCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import StarRating from 'vue-star-rating'
+
+const router = useRouter();
+const route = useRoute()
+const api = useApiStore();
+const config = useRuntimeConfig();
+
+const props = defineProps<{
+    data: {
+        rating: number;
+        name: string;
+        profile: string;
+        id: string;
+    }
+}>();
+
+
+
+</script>
+
+<template>
+    <button @click="router.push(`/profile/${data.id}`)"
+        class="bg-white shadow-lg rounded-xl w-full mx-5 my-3 p-3 flex flex-row">
+        <div :style="`background-image: url('${data.profile}');`" class="w-20 h-20 rounded-full bg-cover bg-center"></div>
+        <div class="flex flex-col h-20 items-start justify-center gap-1 ml-4">
+            <h2 class="text-xl text-gray-900">{{ data.name }}</h2>
+
+            <star-rating star-size="20" :rating="data.rating" :show-rating="false"></star-rating>
+        </div>
+    </button>
+</template>
