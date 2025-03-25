31c2301793129da9f1bc7138b972d5b0e44e33b6
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-03-25 19:12:52 +0700

    feat: make filter button support location field

--- components/FilterButton.vue
+++ components/FilterButton.vue
@@ -17,6 +17,7 @@ const emit = defineEmits<{
 
 const startDate = ref("");
 const endDate = ref("");
+const location = ref("")
 const selectedCategory = ref(); // Default selected category
 const dropdownOpen = ref(false);
 const activeDays = ref<string[]>([]);
@@ -90,6 +91,9 @@ const applyFilter = () => {
       params.append("repeatedDay", activeDays.value.join(","));
     }
   }
+  if (props.filterOptions?.isSelectingLocation && location.value) {
+    params.append("location", location.value);
+  }
 
   emit("applyFilter", { url: `${params.toString()}` });
   showConfirmDialog.value = false;
@@ -105,17 +109,11 @@ const closeFilter = () => {
 </script>
 
 <template>
-  <div
-    class="flex items-center justify-center p-[10px] border border-stroke rounded-[6px]"
-    @click="openFilter"
-  >
+  <div class="flex items-center justify-center p-[10px] border border-stroke rounded-[6px]" @click="openFilter">
     <Icon icon="solar:filter-linear" class="w-[16px] h-[16px] text-black" />
   </div>
 
-  <div
-    v-if="showConfirmDialog"
-    class="fixed inset-0 flex items-center justify-center z-[102]"
-  >
+  <div v-if="showConfirmDialog" class="fixed inset-0 flex items-center justify-center z-[102]">
     <div class="gap-[20px] bg-white p-[25px] rounded-lg shadow-lg w-[346px]">
       <div class="flex justify-between items-center font-medium">
         <span>Sort & Filter</span>
@@ -125,30 +123,18 @@ const closeFilter = () => {
       <hr class="border-t border-gray-300 my-4" />
 
       <!-- Category -->
-      <div
-        v-if="props.filterOptions?.isCategorizing"
-        class="flex relative justify-between mb-2"
-      >
+      <div v-if="props.filterOptions?.isCategorizing" class="flex relative justify-between mb-2">
         <span class="text-gray-500">Category</span>
-        <button
-          @click="toggleDropdown"
-          class="text-gray-500 focus:outline-none"
-        >
+        <button @click="toggleDropdown" class="text-gray-500 focus:outline-none">
           &gt;
         </button>
 
         <!-- Dropdown -->
-        <div
-          v-if="dropdownOpen"
-          class="absolute top-8 right-0 bg-white border border-gray-300 rounded-lg shadow-lg w-60"
-        >
+        <div v-if="dropdownOpen"
+          class="absolute z-50 top-8 right-0 bg-white border border-gray-300 rounded-lg shadow-lg w-60">
           <ul>
-            <li
-              v-for="category in availableCategories"
-              :key="category"
-              @click="selectCategory(category)"
-              class="px-4 py-2 hover:bg-gray-100 cursor-pointer text-sm"
-            >
+            <li v-for="category in availableCategories" :key="category" @click="selectCategory(category)"
+              class="px-4 py-2 hover:bg-gray-100 cursor-pointer text-sm">
               {{ category }}
             </li>
           </ul>
@@ -157,78 +143,48 @@ const closeFilter = () => {
 
       <!-- Category Input -->
       <div v-if="selectedCategory" class="flex gap-2">
-        <div
-          class="bg-black text-white px-3 py-1 rounded-full flex items-center gap-2 text-sm"
-        >
+        <div class="bg-black text-white px-3 py-1 rounded-full flex items-center gap-2 text-sm">
           {{ selectedCategory }}
         </div>
       </div>
 
-      <div
-        v-if="props.filterOptions?.isSelectingDateRange"
-        class="mt-4 flex flex-col gap-2"
-      >
+      <div v-if="props.filterOptions?.isSelectingDateRange" class="mt-4 flex flex-col gap-2">
         <h2 class="text-gray-500">Date Range</h2>
         <div class="flex gap-2">
           <div>
             <label class="block text-sm font-medium">Start Date</label>
-            <input
-              v-model="startDate"
-              type="date"
-              class="w-full border rounded-md p-1.5 text-sm text-gray-500"
-            />
+            <input v-model="startDate" type="date" class="w-full border rounded-md p-1.5 text-sm text-gray-500" />
           </div>
           <div>
             <label class="block text-sm font-medium">End Date</label>
-            <input
-              v-model="endDate"
-              type="date"
-              class="w-full border rounded-md p-1.5 text-sm text-gray-500"
-            />
+            <input v-model="endDate" type="date" class="w-full border rounded-md p-1.5 text-sm text-gray-500" />
           </div>
         </div>
       </div>
 
       <div v-if="props.filterOptions?.isSelectingPriceRange">
         <h2 class="text-gray-500 mt-4">Choose Price</h2>
-        <DoubleSlider
-          v-model="sliderValue"
-          :min="0"
-          :max="10000"
-          :minRange="5"
-          class="custom-slider"
-        />
+        <DoubleSlider v-model="sliderValue" :min="0" :max="10000" :minRange="5" class="custom-slider" />
         <p>Selected Range: {{ sliderValue[0] }} - {{ sliderValue[1] }}</p>
       </div>
 
       <div v-if="props.filterOptions?.isSelectingDuration">
         <h2 class="text-gray-500 mt-4">Select Duration (Minute)</h2>
-        <DoubleSlider
-          v-model="sliderValue"
-          :min="0"
-          :max="600"
-          :minRange="5"
-          class="custom-slider"
-        />
+        <DoubleSlider v-model="sliderValue" :min="0" :max="600" :minRange="5" class="custom-slider" />
         <p>Selected Range: {{ sliderValue[0] }} - {{ sliderValue[1] }}</p>
       </div>
 
       <!-- Location Filter -->
       <div v-if="props.filterOptions?.isSelectingLocation">
         <h2 class="text-gray-500 mt-4">Location</h2>
-        <input
-          type="text"
-          class="w-full border rounded-md p-1.5 text-sm text-gray-500"
-        />
+        <input v-model="location" type="text" class="w-full border rounded-md p-1.5 text-sm text-gray-500" />
       </div>
 
       <!-- Selecting Day -->
       <div v-if="props.filterOptions?.isSelectingActiveDays">
         <h2 class="text-gray-500 mt-4">Avaliable Date</h2>
         <div class="flex justify-between mt-1.5">
-          <button
-            v-for="(day, index) in days"
-            :key="index"
+          <button v-for="(day, index) in days" :key="index"
             class="flex justify-center w-[36px] py-[10px] px-[12px] border rounded-[5px] text-[12px] font-light cursor-pointer"
             :class="{
               'border-primary text-primary': activeDays.includes(
@@ -237,9 +193,7 @@ const closeFilter = () => {
               'border-stroke text-placeHolder': !activeDays.includes(
                 day.name.toUpperCase()
               ),
-            }"
-            @click="toggleDay(day.name)"
-          >
+            }" @click="toggleDay(day.name)">
             {{ day.name }}
           </button>
         </div>
