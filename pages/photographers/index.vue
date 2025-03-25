b19545c812315b982b607fb14669345b34dbed05
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-03-25 19:15:15 +0700

    feat: search photographer with filter

--- pages/photographers/index.vue
+++ pages/photographers/index.vue
@@ -1,17 +1,54 @@
 <script setup lang="ts">
 import { type UserResponse } from '~/types/api';
+import { type SearchPhotographerParams } from '~/types/api_manual';
 
 
 const router = useRouter();
 const route = useRoute();
 const api = useApiStore();
 
-const photographers = ref<UserResponse[]>([]);
+const photographers = ref<UserResponse[] | null>(null);
 
+const searchQuery = ref("");
+const updateSearchQuery = (q: string) => {
+    searchQuery.value = q.length === 0 ? "" : q.split("search=")[1];
+    loadPhotographers();
+}
+
+const filterQuery = ref<SearchPhotographerParams>({});
+const updateFilters = (q: string) => {
+    if (q.length === 0) {
+        filterQuery.value = {};
+        loadPhotographers();
+        return;
+    }
+    const parts = q.split("&");
+    const res: any = {};
+    for (let part of parts) {
+        const [key, value] = part.split("=");
+        res[key] = value;
+    }
+    filterQuery.value = res;
+    loadPhotographers();
+}
+
+const timeout = ref<ReturnType<typeof setTimeout> | null>(null);
+const loadPhotographers = async () => {
+    if (timeout.value !== null) {
+        clearTimeout(timeout.value);
+    }
+
+    setTimeout(async () => {
+        photographers.value = null;
+
+        const payload = { ...filterQuery.value };
+        if (searchQuery.value.length > 0) payload.name = searchQuery.value;
+        photographers.value = await api.searchPhotographer(payload);
+    }, 500)
+}
 
 onMounted(async () => {
-    photographers.value = await api.searchPhotographer({});
-    console.log(photographers.value);
+    loadPhotographers();
 })
 </script>
 
@@ -24,9 +61,10 @@ onMounted(async () => {
             </div>
         </div>
 
-        <SearchBar role="guest" />
+        <SearchBar @update:search="updateSearchQuery" @update:filter="updateFilters"
+            :filter-options="{ isCategorizing: true, isSelectingLocation: true, isSelectingPriceRange: true }" />
 
-        <div v-if="photographers.length === 0" class="w-full p-4">
+        <div v-if="photographers === null" class="w-full p-4">
             <div v-for="i in 5" :key="i" class="w-full h-32 rounded-xl mt-4 bg-gray-300 animate-pulse"></div>
         </div>
 
