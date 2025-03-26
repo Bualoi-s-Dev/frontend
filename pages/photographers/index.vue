86a45834643e9e89768a54db4913ae1f82fcac4f
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-03-27 03:51:23 +0700

    fix: search photographer page use original implementation

--- pages/photographers/index.vue
+++ pages/photographers/index.vue
@@ -13,88 +13,66 @@ const searchQuery = ref("");
 const photographers = ref<UserResponse[] | null>(null);
 
 const updateSearchQuery = (q: string) => {
-    searchQuery.value = q.length === 0 ? "" : q.split("search=")[1];
-    loadPhotographers();
+  searchQuery.value = q.length === 0 ? "" : q.split("search=")[1];
+  loadPhotographers();
 }
 
 const filterQuery = ref<SearchPhotographerParams>({});
 const updateFilters = (q: string) => {
-    if (q.length === 0) {
-        filterQuery.value = {};
-        loadPhotographers();
-        return;
-    }
-    const parts = q.split("&");
-    const res: any = {};
-    for (let part of parts) {
-        const [key, value] = part.split("=");
-        res[key] = value;
-    }
-    filterQuery.value = res;
+  if (q.length === 0) {
+    filterQuery.value = {};
     loadPhotographers();
+    return;
+  }
+  const parts = q.split("&");
+  const res: any = {};
+  for (let part of parts) {
+    const [key, value] = part.split("=");
+    res[key] = value;
+  }
+  filterQuery.value = res;
+  loadPhotographers();
 }
 
 const timeout = ref<ReturnType<typeof setTimeout> | null>(null);
 const loadPhotographers = async () => {
-    if (timeout.value !== null) {
-        clearTimeout(timeout.value);
-    }
+  if (timeout.value !== null) {
+    clearTimeout(timeout.value);
+  }
 
-    setTimeout(async () => {
-        photographers.value = null;
+  setTimeout(async () => {
+    photographers.value = null;
 
-        const payload = { ...filterQuery.value };
-        if (searchQuery.value.length > 0) payload.name = searchQuery.value;
-        photographers.value = await api.searchPhotographer(payload);
-    }, 500)
+    const payload = { ...filterQuery.value };
+    if (searchQuery.value.length > 0) payload.name = searchQuery.value;
+    photographers.value = await api.searchPhotographer(payload);
+  }, 500)
 }
 
 onMounted(async () => {
-    loadPhotographers();
+  loadPhotographers();
 })
 </script>
 
 <template>
-    <div class="mt-6 flex flex-col">
-        <div class="flex flex-row justify-between mx-5.5">
-            <div class="flex flex-row items-center gap-3">
-                <BackButton />
-                <h1 class="text-md">Photographers</h1>
-            </div>
-        </div>
-
-        <SearchBar @update:search="updateSearchQuery" @update:filter="updateFilters"
-            :filter-options="{ isCategorizing: true, isSelectingLocation: true, isSelectingPriceRange: true }" />
-
-        <div v-if="photographers === null" class="w-full p-4">
-            <div v-for="i in 5" :key="i" class="w-full h-32 rounded-xl mt-4 bg-gray-300 animate-pulse"></div>
-        </div>
-
-        <div v-for="photographer, index in photographers" :key="photographer.id" class="flex items-center">
-            <PhotographerCard :data="photographer" />
-        </div>
+  <div class="mt-6 flex flex-col">
+    <div class="flex flex-row justify-between mx-5.5">
+      <div class="flex flex-row items-center gap-3">
+        <BackButton />
+        <h1 class="text-md">Photographers</h1>
+      </div>
     </div>
 
-    <SearchBar
-      search-key="name"
-      role="guest"
-      @update:search="searchQuery = $event"
-      @update:filter="filterUrl = $event"
-    />
-    {{ searchQuery }}
-    <div v-if="photographers.length === 0" class="w-full p-4">
-      <div
-        v-for="i in 5"
-        :key="i"
-        class="w-full h-32 rounded-xl mt-4 bg-gray-300 animate-pulse"
-      ></div>
+    <SearchBar search-key="search" @update:search="updateSearchQuery" @update:filter="updateFilters"
+      :filter-options="{ isCategorizing: true, isSelectingLocation: true, isSelectingPriceRange: true }" />
+
+    <div v-if="photographers === null" class="w-full p-4">
+      <div v-for="i in 5" :key="i" class="w-full h-32 rounded-xl mt-4 bg-gray-300 animate-pulse"></div>
     </div>
-    <div
-      v-for="(photographer, index) in photographers"
-      :key="photographer.id"
-      class="flex items-center"
-    >
+
+    <div v-for="photographer, index in photographers" :key="photographer.id" class="flex items-center">
       <PhotographerCard :data="photographer" />
     </div>
   </div>
+
 </template>
