f33419cab4fe815d7a189c4c74fffa83eca541e9
neonaht <71958540+neonaht@users.noreply.github.com>
2025-04-08 19:19:51 +0700

    Fix: issues #48 #49 #50

--- components/TransactionCard.vue
+++ components/TransactionCard.vue
@@ -6,26 +6,29 @@ const route = useRoute()
 const api = useApiStore();
 const config = useRuntimeConfig();
 
-const paymentData = ref<PaymentResponse | undefined>();
+// const paymentData = ref<PaymentResponse | undefined>();
 
 const prop = withDefaults(
   defineProps<{
+    pageId: string | undefined;
     role: UserRole | undefined;
-    paymentId: string;
+    paymentData: PaymentResponse;
   }>(),
   {
-    role: UserRole.Guest
+    pageId: 'Transaction',
+    role: UserRole.Guest, 
+    paymentData: undefined
   }
 );
 
-const fetchNewPaymentData = async() => {
-    const response = await api.fetchPayment(prop.paymentId);
-    paymentData.value = response;
-}
+// const fetchNewPaymentData = async() => {
+//     const response = await api.fetchPayment(prop.paymentId);
+//     paymentData.value = response;
+// }
 
-onMounted(async () => {
-    fetchNewPaymentData();
-})
+// onMounted(async () => {
+//     fetchNewPaymentData();
+// })
 
 const formattedDate = (time: string | undefined) => {
     if(time == undefined) return 'Error no time parameter';
@@ -40,26 +43,38 @@ const formattedTime = (time: string | undefined) => {
 }
 
 const onPay = async () => {
-    router.push({ path: `/payment/order/${paymentData.value?.payment.id}` });
+    router.push({ path: `/payment/order/${prop.paymentData.payment.id}` });
 }
 
 </script>
 
 <template>
-    <div class="bg-neutral-100 w-full shadow-md rounded-lg mx-5 my-3 p-3">
-        <div class="flex flex-col gap-1">
-            <h2 class="text-lg font-semibold text-gray-900">{{ paymentData?.appointment?.packageName }}</h2>
+    <div class="bg-neutral-100 w-full shadow-md rounded-lg mx-5 my-3">
+        <div class="bg-gray-400 p-3">
+            <h2 class="text-lg font-semibold text-gray-900">{{ paymentData?.appointment?.packageName }} / {{ paymentData?.appointment?.subpackageName }}</h2>
+        </div>
+        <div class="flex flex-col gap-1 p-3">
             <p class="text-gray-600 mt-1">Date : <span class="font-medium">{{ formattedDate(paymentData?.appointment?.startTime) }}</span></p>
-            <p class="text-gray-600 mt-1">Time : <span class="font-medium">{{ formattedTime(paymentData?.appointment?.startTime) }} - {{ formattedTime(paymentData?.appointment?.endTime) }}</span></p>
+            <p class="text-gray-600">Time : <span class="font-medium">{{ formattedTime(paymentData?.appointment?.startTime) }} - {{ formattedTime(paymentData?.appointment?.endTime) }}</span></p>
             <p class="text-gray-600">From <span class="font-semibold">{{ paymentData?.appointment?.customerName }}</span> To <span class="font-semibold">{{ paymentData?.appointment?.photographerName }}</span></p>
-            <p class="text-gray-600">Total : <span class="text-lg font-semibold text-red-500">{{ paymentData?.appointment?.price }} ฿</span></p>
-            <div v-if="paymentData?.payment.customer.status != PaymentStatus.Unpaid">
-                <p v-if="role == UserRole.Customer" class="text-gray-900">Status : <span class="font-medium">{{ paymentData?.payment.customer.status }}</span></p>
+            <div class="w-full h-0.5 bg-gray-400 mt-2 mb-2"></div>
+            <div class="flex flex-row justify-between items-center">
+                <div v-if="role == UserRole.Customer">
+                    <p v-if="paymentData?.payment.customer.status == PaymentStatus.Unpaid" class="text-gray-900">Status : <span class="font-medium text-amber-600">{{ paymentData?.payment.customer.status }}</span></p>
+                    <p v-if="paymentData?.payment.customer.status == PaymentStatus.Paid" class="text-gray-900">Status : <span class="font-medium text-green-600">{{ paymentData?.payment.customer.status }}</span></p>
+                </div>
+                <div v-if="role == UserRole.Photographer">
+                    <p v-if="paymentData?.payment.photographer.status == PaymentStatus.Wait" class="text-gray-900">Status : <span class="font-medium text-amber-600">{{ paymentData?.payment.photographer.status }}</span></p>
+                    <p v-if="paymentData?.payment.photographer.status == PaymentStatus.InProcess" class="text-gray-900">Status : <span class="font-medium text-blue-600">{{ paymentData?.payment.photographer.status }}</span></p>
+                    <p v-if="paymentData?.payment.photographer.status == PaymentStatus.Completed" class="text-gray-900">Status : <span class="font-medium text-green-600">{{ paymentData?.payment.photographer.status }}</span></p>
+                </div>
+                <div>
+                    <p class="text-gray-900"><span class="text-lg font-semibold">{{ paymentData?.appointment?.price }} ฿</span></p>
+                </div>
             </div>
-            <p v-if="role == UserRole.Photographer" class="text-gray-900">Status : <span class="font-medium">{{ paymentData?.payment.photographer.status }}</span></p>
             <div class="flex flex-row gap-2">
                 <div class="flex flex-row gap-2">
-                    <Button v-if="role == UserRole.Customer && paymentData?.payment.customer.status == PaymentStatus.Unpaid" @click="onPay" height="h-9" button-options="border border-stroke rounded-md px-8">Pay</Button>
+                    <Button v-if="role == UserRole.Customer && paymentData?.payment.customer.status == PaymentStatus.Unpaid && pageId == 'ToPay'" class="mt-1" @click="onPay" height="h-9" button-options="border border-stroke rounded-md px-8">Pay</Button>
                 </div>
             </div>
         </div>
