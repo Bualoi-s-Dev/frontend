2a304f2a52f657430d466da9c89c473e87c709a5
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-04-13 21:54:53 +0700

    build: install vue date picker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  build: {
+    transpile: ['@vuepic/vue-datepicker']
+  },
   runtimeConfig: {
     public: {
       firebaseConfig: {
